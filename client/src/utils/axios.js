23d4dc5819a88c6a98f8a8011caec77876bbd3b5
Billuboy <billuboy1718@gmail.com>
2022-02-02 17:46:49 +0530

    axios patched

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_BACK_END_PORT}/api`,
+  baseURL: `http://${process.env.REACT_APP_AXIOS_HOST}:${process.env.REACT_APP_BACK_END_PORT}/api`,
   withCredentials: true,
 });
