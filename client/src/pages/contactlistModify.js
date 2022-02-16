77b417bd7451a1a2f0916957d54a8aa40e8ff382
Billuboy <billuboy1718@gmail.com>
2022-02-16 15:55:00 +0530

    Linting error resolved

--- client/src/pages/contactlistModify.js
+++ client/src/pages/contactlistModify.js
@@ -6,176 +6,161 @@ import axios from '../utils/axios';
 import { useAuth } from '../hooks/useAuth';
 
 export default function ContactListCreate() {
-    const [loading, setLoading] = useState(true);
-    const [contactList, setContactList] = useState('');
-    const [disabled, setDisabled] = useState(false);
-    const [contactlistarr, setcontactlistarr] = useState([]);
-    const [selectedUsers, setSelectedUsers] = useState([]);
-    const [userlist, setuserlist] = useState([]);
-    const [contactlistName, setcontactlistName] = useState('');
-    const selectedUserIds = useRef(new Set());
+  const [loading, setLoading] = useState(true);
+  const [contactList, setContactList] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  const [contactlistarr, setcontactlistarr] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [userlist, setuserlist] = useState([]);
+  const [contactlistName, setcontactlistName] = useState('');
+  const selectedUserIds = useRef(new Set());
 
-    useEffect(() => {
-        (async () => {
-            const { data: users } = await axios.get('/user/ptt');
-            const { data: contactlists } = await axios.get('/contactlist/');
-            setuserlist(users);
-            setcontactlistarr(contactlists);
-            console.log(users);
-            console.log(contactlists);
-            setLoading(false);
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      const { data: users } = await axios.get('/user/ptt');
+      const { data: contactlists } = await axios.get('/contactlist/');
+      setuserlist(users);
+      setcontactlistarr(contactlists);
+      console.log(users);
+      console.log(contactlists);
+      setLoading(false);
+    })();
+  }, []);
 
-    const reset = () => {
-        setContactList('');
+  const reset = () => {
+    setContactList('');
+  };
+
+  function onSelect(users) {
+    let selected = [];
+    selectedUserIds.current.forEach((id) => {
+      selected.push(users.filter((user) => user.id === id)[0]);
+    });
+    setSelectedUsers(selected);
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setDisabled(true);
+
+    const users = [];
+    selectedUserIds.current.forEach((id) => users.push(id));
+    const data = {
+      name: contactlistName,
+      userIds: users,
     };
 
-    function onSelect(users) {
-        let selected = [];
-        selectedUserIds.current.forEach((id) => {
-            selected.push(users.filter((user) => user.id === id)[0]);
-        });
-        setSelectedUsers(selected);
+    try {
+      await axios.put(`/contactlist/${contactList}`, data);
+      reset();
+    } catch (err) {
+      console.log(err.response.data);
     }
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        setDisabled(true);
+    setDisabled(false);
+  };
 
-        const users = [];
-        selectedUserIds.current.forEach((id) => users.push(id));
-        const data = {
-            name: contactlistName,
-            userIds: users,
-        };
-
-        try {
-            await axios.put(`/contactlist/${contactList}`, data);
-            reset();
-        } catch (err) {
-            console.log(err.response.data);
-        }
-
-        setDisabled(false);
-    };
-
-    const SelectAcc = ({ users }) => {
-        return (
-            <div>
-                <span>PTT User Accounts</span>
-                <br />
-                <div className="comp">
-                    <div className="accbox">
-                        {users.map((val, index) => {
-                            return (
-                                <div key={val.id}>
-                                    <input
-                                        type="checkbox"
-                                        id="subitem"
-                                        name="selection"
-                                        defaultChecked={selectedUserIds.current.has(
-                                            val.id
-                                        )}
-                                        onClick={() => {
-                                            selectedUserIds.current.has(val.id)
-                                                ? selectedUserIds.current.delete(
-                                                      val.id
-                                                  )
-                                                : selectedUserIds.current.add(
-                                                      val.id
-                                                  );
-                                        }}
-                                    />
-                                    <label for="selection">
-                                        {val.display_name}
-                                    </label>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <button type="button" onClick={() => onSelect(users)}>
-                        &nbsp;&nbsp; &gt; &gt; &nbsp;&nbsp;
-                    </button>
-                    <div className="accbox">
-                        {(selectedUsers.length &&
-                            selectedUsers.map((val) => {
-                                return (
-                                    <div key={val.id}>{val.display_name}</div>
-                                );
-                            })) ||
-                            null}
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
-    if (loading) {
-        return (
-            <div className="passback">
-                <div
-                    className="spinner-border text-primary"
-                    role="status"
-                ></div>
-            </div>
-        );
-    }
+  const SelectAcc = ({ users }) => {
     return (
-        <form className="passback" onSubmit={onSubmit}>
-            <div style={{ fontWeight: 'bolder', fontSize: '4vh' }}>
-                MODIFY CONTACT LIST
-            </div>
-            <br />
-            <div className="formarea">
-                <div>
-                    <span>
-                        <label htmlFor="username">
-                            Contact list Name: &nbsp;
-                        </label>
-                    </span>
-                    <input
-                        type="text"
-                        id="username"
-                        onChange={(event) => {
-                            setcontactlistName(event.target.value);
-                        }}
-                        value={contactlistName}
-                        required
-                    />
+      <div>
+        <span>PTT User Accounts</span>
+        <br />
+        <div className="comp">
+          <div className="accbox">
+            {users.map((val, index) => {
+              return (
+                <div key={val.id}>
+                  <input
+                    type="checkbox"
+                    id="subitem"
+                    name="selection"
+                    defaultChecked={selectedUserIds.current.has(val.id)}
+                    onClick={() => {
+                      selectedUserIds.current.has(val.id)
+                        ? selectedUserIds.current.delete(val.id)
+                        : selectedUserIds.current.add(val.id);
+                    }}
+                  />
+                  <label for="selection">{val.display_name}</label>
                 </div>
-                <br />
-                <div>
-                    <span>
-                        <label htmlFor="company">
-                            Select Contact-List :&nbsp;&nbsp;&nbsp;{' '}
-                        </label>
-                    </span>
-                    <select
-                        id="company"
-                        onChange={(event) => {
-                            setContactList(event.target.value);
-                        }}
-                        value={contactList}
-                        required
-                    >
-                        <option value={0}>Select Talk-Group</option>
-                        {contactlistarr.map((val) => {
-                            return (
-                                <option key={val.id} value={val.id}>
-                                    {val.display_name}
-                                </option>
-                            );
-                        })}
-                    </select>
-                </div>
-                <br />
-                <SelectAcc users={userlist} />
-            </div>
-            <br />
-            <button type="submit" disabled={disabled}>
-                Save
-            </button>
-        </form>
+              );
+            })}
+          </div>
+          <button type="button" onClick={() => onSelect(users)}>
+            &nbsp;&nbsp; &gt; &gt; &nbsp;&nbsp;
+          </button>
+          <div className="accbox">
+            {(selectedUsers.length &&
+              selectedUsers.map((val) => {
+                return <div key={val.id}>{val.display_name}</div>;
+              })) ||
+              null}
+          </div>
+        </div>
+      </div>
     );
+  };
+
+  if (loading) {
+    return (
+      <div className="passback">
+        <div className="spinner-border text-primary" role="status"></div>
+      </div>
+    );
+  }
+  return (
+    <form className="passback" onSubmit={onSubmit}>
+      <div style={{ fontWeight: 'bolder', fontSize: '4vh' }}>
+        MODIFY CONTACT LIST
+      </div>
+      <br />
+      <div className="formarea">
+        <div>
+          <span>
+            <label htmlFor="username">Contact list Name: &nbsp;</label>
+          </span>
+          <input
+            type="text"
+            id="username"
+            onChange={(event) => {
+              setcontactlistName(event.target.value);
+            }}
+            value={contactlistName}
+            required
+          />
+        </div>
+        <br />
+        <div>
+          <span>
+            <label htmlFor="company">
+              Select Contact-List :&nbsp;&nbsp;&nbsp;{' '}
+            </label>
+          </span>
+          <select
+            id="company"
+            onChange={(event) => {
+              setContactList(event.target.value);
+            }}
+            value={contactList}
+            required
+          >
+            <option value={0}>Select Talk-Group</option>
+            {contactlistarr.map((val) => {
+              return (
+                <option key={val.id} value={val.id}>
+                  {val.display_name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <br />
+        <SelectAcc users={userlist} />
+      </div>
+      <br />
+      <button type="submit" disabled={disabled}>
+        Save
+      </button>
+    </form>
+  );
 }
